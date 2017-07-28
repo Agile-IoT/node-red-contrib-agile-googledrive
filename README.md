6d1c44239c53c2c1e7997352fc164693cc52a39b
Philippe Krief <philippe.krief@eclipse.org>
2017-07-28 17:06:19 +0200

    Ading missing copyrights

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
+<!--
+# Copyright (C) 2017 ROMBIT.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+# 
+# Contributors:
+#     ROMBIT - initial API and implementation
+-->
+
 # node-red-contrib-agile-googledrive
 
 <p>A node to upload files to the google drive API. </p>
--- google.js
+++ google.js
@@ -1,14 +1,12 @@
 /*******************************************************************************
- * Copyright (C) 2017 Orange.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ *Copyright (C) 2017 ROMBIT.
+ *All rights reserved. This program and the accompanying materials
+ *are made available under the terms of the Eclipse Public License v1.0
+ *which accompanies this distribution, and is available at
+ *http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors:
- *     Rombit - initial implementation
- *
- * Based on https://github.com/node-red/node-red-web-nodes
+ *Contributors:
+ *    ROMBIT - initial API and implementation
  ******************************************************************************/
 /*
  * To change this license header, choose License Headers in Project Properties.
